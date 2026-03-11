dc298ed0c142497974dd29cd6a0ad5fe551bad69
sirinoglutaylan <taylansirinoglu@gmail.com>
2026-03-11 22:40:53 +0300

    Add PostgreSQL integration and implement /cities endpoint

--- app.py
+++ app.py
@@ -1,23 +1,13 @@
-from flask import Flask
- 
-app = Flask(__name__)
- 
-@app.route("/hello")
-
-def hello():
-
-    return "Hello! Favorite City Counter API"
- 
-if __name__ == "__main__":
-
-    app.run(port=5000)
-
-@app.route("/cities")
-def cities():
-    return "Cities endpoint"
-
+from flask import Flask, jsonify
 import psycopg2
- 
+
+app = Flask(__name__)
+
+@app.route("/hello")
+def hello():
+    return "Hello! Favorite City Counter API"
+
+
 def get_db_connection():
     conn = psycopg2.connect(
         host="localhost",
@@ -27,18 +17,23 @@ def get_db_connection():
     )
     return conn
 
+
 @app.route("/cities", methods=["GET"])
 def get_cities():
- 
+
     conn = get_db_connection()
     cur = conn.cursor()
- 
+
     cur.execute("SELECT name FROM cities")
     rows = cur.fetchall()
- 
+
     cur.close()
     conn.close()
- 
+
     cities = [row[0] for row in rows]
- 
+
     return jsonify(cities)
+
+
+if __name__ == "__main__":
+    app.run(port=5000)
