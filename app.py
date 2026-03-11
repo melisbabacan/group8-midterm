2c8b2031eb1e24e7a0ce992001b9d943277ab687
sirinoglutaylan <taylansirinoglu@gmail.com>
2026-03-11 21:28:22 +0300

    add hello endpoint

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+@app.route("/hello")
+
+def hello():
+
+    return "Hello! Favorite City Counter API"
+ 
+if __name__ == "__main__":
+
+    app.run(port=5000)
+ 
